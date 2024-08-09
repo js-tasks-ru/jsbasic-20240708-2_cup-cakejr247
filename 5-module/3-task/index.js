ac9d21fb35084c490d5f795771ac32062d1a4a1e
c2h602 <cup.cakejr247@gmail.com>
2024-08-09 15:31:57 +0300

    Модуль 5. Задача 3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,45 @@
 function initCarousel() {
-  // ваш код...
+  const carouselInner = document.querySelector('.carousel__inner'); 
+  const carouselRight = document.querySelector('.carousel__arrow_right');
+  const carouselLeft = document.querySelector('.carousel__arrow_left');
+  
+  const slidesCount = carouselInner.children.length; 
+  const slideWidth = carouselInner.offsetWidth;
+  
+  let currentSlide = 0;
+
+  carouselLeft.style.display = 'none';
+
+  carouselRight.addEventListener('click', () => {
+    if (currentSlide < slidesCount - 1) {
+      currentSlide++;
+      updateCarousel();
+    }
+  });
+
+  carouselLeft.addEventListener('click', () => {
+    if (currentSlide > 0) {
+      currentSlide--;
+      updateCarousel();
+    }
+  });
+
+  function updateCarousel() {
+    carouselInner.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+
+    if (currentSlide === 0) {
+      carouselLeft.style.display = 'none';
+    } else {
+      carouselLeft.style.display = '';
+    }
+
+    if (currentSlide === slidesCount - 1) {
+      carouselRight.style.display = 'none';
+    } else {
+      carouselRight.style.display = '';
+    }
+  }
+
 }
+
+initCarousel();
